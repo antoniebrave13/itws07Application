abd6787d620fcaef3aa5a1ce5ae4562d616e5d98
Yuni <irmabagsic6@gmail.com>
2024-08-30 16:28:07 +0800

    Merge branch 'main' of https://github.com/antoniebrave13/itws07Application
    add my component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { KingComponentComponent } from './king-component/king-component.componen
 import { AlcheComponentComponent } from './alche-component/alche-component.component';
 import { FroilanComponentComponent } from './froilan-component/froilan-component.component';
 import { YuniComponentComponent } from './yuni-component/yuni-component.component';
+import { JennaComponentComponent } from './jenna-component/jenna-component.component';
 
 
 export const routes: Routes = [
@@ -27,7 +28,9 @@ export const routes: Routes = [
     { path: 'arlene-component', component: ArleneComponentComponent },
     { path: 'alche-component', component: AlcheComponentComponent },
     { path: 'froilan-component', component: FroilanComponentComponent },
-    { path: 'yuni-component', component: YuniComponentComponent }
+    { path: 'yuni-component', component: YuniComponentComponent },
+    { path: 'jenna-component', component: JennaComponentComponent },
+
   ];
   
 
