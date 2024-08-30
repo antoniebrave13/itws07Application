6c078b7b31d0d83077c2a8a1e0fa1595f0db3b85
antonio.bravo <13165318+MrSimplevia2018@users.noreply.github.com>
2024-08-30 15:39:19 +0800

    merged conflicts
    merged conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
 import { BravoComponentComponent } from './bravo-component/bravo-component.component';
-<<<<<<< Updated upstream
 import { AngelineComponentComponent } from './angeline-component/angeline-component.component';
 
-export const routes: Routes = [
-    { path: 'bravo-component', component: BravoComponentComponent }
-    {path: 'angeline-component', component: AngelineComponentComponent}
-=======
 import { BuenoComponentComponent } from './bueno-component/bueno-component.component';
 
 export const routes: Routes = [
     { path: 'bravo-component', component: BravoComponentComponent },
+    { path: 'angeline-component', component: AngelineComponentComponent},
+    { path: 'bravo-component', component: BravoComponentComponent },
     { path: 'bueno-component', component: BuenoComponentComponent }
->>>>>>> Stashed changes
-  ];
-  
+]
 
