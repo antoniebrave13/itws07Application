3ae8d1e0d98a0f2d973e41a3aefed43f779e457f
Yuni <irmabagsic6@gmail.com>
2024-08-30 15:40:42 +0800

    add my own component
    add my own component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { BravoComponentComponent } from './bravo-component/bravo-component.component';
+import { YuniComponentComponent } from './yuni-component/yuni-component.component';
 
 export const routes: Routes = [
-    { path: 'bravo-component', component: BravoComponentComponent }
+    { path: 'bravo-component', component: BravoComponentComponent },
+    { path: 'yuni-component', component: YuniComponentComponent }
+    
   ];
+  
 
 
---
+++ src/app/yuni-component/yuni-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yuni-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './yuni-component.component.html',
+  styleUrl: './yuni-component.component.css'
+})
+export class YuniComponentComponent {
+
+}
