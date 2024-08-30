f2c0c6dc198f7b1b7adc7c730e10e3ead3288cb0
itsyogurlarlenggg <arlenedesalisa@gmail.com>
2024-08-30 15:58:15 +0800

    edit my own components
    edit my own components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { ElaComponentComponent } from './ela-component/ela-component.component';
 import { MelvinComponentComponent } from './melvin-component/melvin-component.component';
 import { JastineComponentComponent } from './jastine-component/jastine-component.component';
 import { AngelineComponentComponent } from './angeline-component/angeline-component.component';
-
 import { BuenoComponentComponent } from './bueno-component/bueno-component.component';
+import { ArleneComponentComponent } from './arlene-component/arlene-component.component';
 
 export const routes: Routes = [
     { path: 'bravo-component', component: BravoComponentComponent },
@@ -14,6 +14,7 @@ export const routes: Routes = [
     { path: 'melvin-component', component: MelvinComponentComponent},
     { path: 'jastine-component', component: JastineComponentComponent},
     { path: 'angeline-component', component: AngelineComponentComponent},
-    { path: 'bueno-component', component: BuenoComponentComponent }
+    { path: 'bueno-component', component: BuenoComponentComponent },
+    { path: 'arlene-component', component: ArleneComponentComponent }
   ];
 
