d8ad3a453f8bd22ab8de2865c438e17da4759a2c
antonio.bravo <13165318+MrSimplevia2018@users.noreply.github.com>
2024-09-20 13:48:07 +0800

    updated angular app
    updated angular app

--- src/app/bravo-component/bravo-component.component.ts
+++ src/app/bravo-component/bravo-component.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BuenoComponentComponent } from '../bueno-component/bueno-component.component';
 
 @Component({
   selector: 'app-bravo-component',
   standalone: true,
-  imports: [],
+  imports: [BuenoComponentComponent],
   templateUrl: './bravo-component.component.html',
+  // template: 'Sample Template',
   styleUrl: './bravo-component.component.css'
 })
 export class BravoComponentComponent {
